24c68ad36da4c48868eaf8e748efeb12f7a5caf2
sangnam18 <sangnam18@nate.com>
2014-08-09 17:18:53 +0900

    ㅅㅅㅅ

--- src/aaaa/ABCcdcd.java
+++ src/aaaa/ABCcdcd.java
@@ -4,7 +4,7 @@ public class ABCcdcd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1111웹에서 테스트");
+		System.out.println("2221111이클립스");
 	}
 
 }
