9f473111becd865b71ac0f7bf680d6f87111995f
sangnam18 <sangnam18@nate.com>
2014-08-09 17:55:29 +0900

    잘못 된거 수정

--- src/aaaa/ABCcdcd.java
+++ src/aaaa/ABCcdcd.java
@@ -5,8 +5,9 @@ public class ABCcdcd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2221111이클립스222");
-		System.out.println("이클립스2222323");
-		System.out.println("testabcdddasdfasfdasdasdfasdf");
+		System.out.println("이클립스2222323");//야야야야야야
+		System.out.println("3번째 코드 입니다.");
+		
 	}
 
 }
