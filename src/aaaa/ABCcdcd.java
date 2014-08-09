87c3c33da0198ac783d8d920c1a5a73e9c64ba98
sangnam18 <sangnam18@nate.com>
2014-08-09 17:34:16 +0900

    Update ABCcdcd.java

--- src/aaaa/ABCcdcd.java
+++ src/aaaa/ABCcdcd.java
@@ -5,8 +5,8 @@ public class ABCcdcd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2221111이클립스222");
-		System.out.println("이클립스222");
-		야야야야~~
+		System.out.println("이클립스2222323");
+		야야야야~~123123
 	}
 
 }
