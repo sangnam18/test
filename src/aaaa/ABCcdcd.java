2b93822874b5d1df38ab913a7ebc8a6299fc6d33
sangnam18 <sangnam18@nate.com>
2014-08-15 15:14:52 +0900

    Update ABCcdcd.java

--- src/aaaa/ABCcdcd.java
+++ src/aaaa/ABCcdcd.java
@@ -23,7 +23,7 @@ public class ABCcdcd {
 		System.out.println("111서버 테스트1212112");
 		System.out.println("11111111222");
 		System.out.println("local111112222");
-		System.out.println("local");
+		System.out.println("local22222222222222222222222");
 	}
 
 }
